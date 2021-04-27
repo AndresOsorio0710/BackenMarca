308b449c38de4dc7fa61cda7621b9ed83bda8afb
andre <andres.osorio0710@gmail.com>
2021-04-26 22:45:04 -0500

    26-04-2021-2

---
+++ user_app/models/user.py
@@ -0,0 +1,15 @@
+from uuid import uuid4
+from django_paranoid.models import ParanoidModel
+from django.db import models
+from person_app.models import Person
+
+
+class User(ParanoidModel):
+    uuid = models.UUIDField(default=uuid4, editable=False, primary_key=True)
+    person = models.ForeignKey(Person, on_delete=models.PROTECT)
+    name = models.CharField(max_length=50, default='DEFAULT')
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=10, default='DEFAULT')
+    role = models.CharField(max_length=20, default='CLIENT')
+    password = models.TextField()
+    password_old = models.TextField()
